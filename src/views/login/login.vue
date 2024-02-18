0fc5308d5db5e0fb41f7c835df186bf7ce1fb6b0
xueyinhu <13453894389@163.com>
2024-02-18 23:59:47 +0800

    that

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,19 +1,37 @@
 <template>
-  <el-row justify="center" align="middle">
-    <el-col :span="8"/>
-    <el-col :span="8">
-      <div class="login">12344</div>
-    </el-col>
-    <el-col :span="8"/>
-  </el-row>
+  <el-form ref="ruleFormRef" :model="ruleForm" :rules="rules" label-width="120px" class="demo-ruleForm">
+    <el-form-item label="UserName:" prop="username">
+      <el-input v-model="ruleForm.username" type="txt" autocomplete="off"/>
+    </el-form-item>
+    <el-form-item label="PassWord:" prop="password">
+      <el-input v-model="ruleForm.password" type="password" autocomplete="off"/>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="loginFn()">Login</el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
 <script lang="ts" setup>
+  import { reactive, toRefs } from 'vue'
+
+  const state = reactive({
+    ruleForm: {
+      username: '',
+      password: ''
+    }
+  })
+
+  const rules = reactive({
+    username: [{required: true, message: 'Please input User name', trigger: 'blur'}],
+    password: []
+  })
+
+  const loginFn = () => {}
+
+  const { ruleForm } = toRefs(state)
 </script>
 
 <style lang="less" scoped>
-.login {
-  background-color: aqua;
-}
 </style>
 
